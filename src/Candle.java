fdfb45eb471b078c4bd8058f9a3b08abe9dedcfe
DAndreDerr <dandre020727@gmail.com>
2023-08-10 10:17:11 -0400

    final

--- src/Candle.java
+++ src/Candle.java
@@ -4,46 +4,55 @@ public class Candle {
     private int quantity;
 
     public Candle(String name, double burnTime, double price) {
+        // Constructor to initialize the Candle object with the provided name, burn time, and price
         this.name = name;
         this.burnTime = burnTime;
         this.price = price;
-        this.quantity=0;
-
+        this.quantity = 0; // Set the initial quantity to 0
     }
 
     public String getName() {
+        // Getter method to retrieve the name of the candle
         return name;
     }
 
     public void setName(String name) {
+        // Setter method to update the name of the candle
         this.name = name;
     }
 
     public double getBurnTime() {
+        // Getter method to retrieve the burn time of the candle
         return burnTime;
     }
 
     public void setBurnTime(double burnTime) {
+        // Setter method to update the burn time of the candle
         this.burnTime = burnTime;
     }
 
     public double getPrice() {
+        // Getter method to retrieve the price of the candle
         return price;
     }
 
     public void setPrice(double price) {
+        // Setter method to update the price of the candle
         this.price = price;
     }
 
     public double getDollarBurnTime() {
-        return (quantity*price)/burnTime;
+        // Method to calculate and retrieve the dollar per burn time of the candle
+        return (quantity * price) / burnTime;
     }
 
     public int getQuantity() {
+        // Getter method to retrieve the quantity of the candle
         return quantity;
     }
 
     public void setQuantity(int quantity) {
+        // Setter method to update the quantity of the candle
         this.quantity = quantity;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,36 +10,47 @@ public class Main {
     static DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
     public static void main(String[] args) {
+        // Adding Candle objects to the ArrayList
         candleArrayList.add(new Candle("Vanilla Candle", 15, 19.99));
         candleArrayList.add(new Candle("Lavender Candle", 17, 9.69));
         candleArrayList.add(new Candle("TeakWood Candle", 9.5, 14.00));
+
         double totalBurn = 0;
         double totalDollarBurn = 0;
         double totalPrice = 0;
+
+        // Prompting the user to purchase candles
         promptInput();
 
         String receipt = "";
         String divider = "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
-        for (int i = 0; i < candleArrayList.size(); i++) {
 
+        // Looping through the candleArrayList to process purchased candles
+        for (int i = 0; i < candleArrayList.size(); i++) {
             Candle temp = candleArrayList.get(i);
             if (temp.getQuantity() > 0) {
+                // Constructing the receipt string with information about the purchased candle
                 receipt += divider + "\n" + temp.getName() + "\n Amount: " + temp.getQuantity() + "\n Burn Time: " + temp.getBurnTime()
                         + "\n Dollar per burn time: " + decimalFormat.format(temp.getDollarBurnTime()) + "\n price: " + decimalFormat.format(temp.getPrice() * temp.getQuantity());
+
+                // Updating the total burn time, total dollar burn time, and total price
                 totalBurn += temp.getBurnTime();
                 totalDollarBurn += temp.getDollarBurnTime();
                 totalPrice += temp.getPrice();
             }
         }
 
+        // Adding the total burn time, total dollar burn time, and total price to the receipt string
         receipt += divider + "\n Total Burn Time: " + decimalFormat.format(totalBurn) + "\n Total Dollar per Burn Time: " + decimalFormat.format(totalDollarBurn) + "\n Total Price: " + decimalFormat.format(totalPrice);
+
+        // Printing the receipt
         System.out.println(receipt);
     }
 
-
     static public void promptInput() {
-
         Scanner scanner = new Scanner(System.in);
+
+        // Prompting the user to select a candle and enter the quantity of candles to purchase
         System.out.println("Welcome to Dre's Candle Shop. Buy my candles!" +
                 "\n ( Enter a number)" +
                 "\n 1. Vanilla Candle \n 2. Lavender Candle \n 3. TeakWood Candle");
@@ -47,6 +58,7 @@ public class Main {
         System.out.println("How many candles? please enter a number");
         int quantity = Integer.parseInt(scanner.nextLine());
 
+        // Updating the quantity of the selected candle based on the user's input
         if (selection == 1) {
             candleArrayList.get(0).setQuantity(quantity);
         } else if (selection == 2) {
@@ -54,18 +66,18 @@ public class Main {
         } else if (selection == 3) {
             candleArrayList.get(2).setQuantity(quantity);
         }
-        System.out.println("Would you like to purchase another candle? (1 - yes, 0 - no");
+
+        // Prompting the user if they want to purchase another candle
+        System.out.println("Would you like to purchase another candle? (1 - yes, 0 - no)");
         int yesNo = Integer.parseInt(scanner.nextLine());
 
         if (yesNo > 0) {
+            // Recursive call to promptInput() if the user wants to purchase another candle
             promptInput();
         } else {
             return;
         }
 
-
         //scanner.close();
     }
-
-
 }
